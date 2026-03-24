02d26f3ef3ff8899e48d4ed7be8a5a4127c972e6
KalaaCode <mistynmika@gmail.com>
2026-03-24 17:40:33 +0400

    Items groups YAY

--- src/main/java/net/kalaa/rhodochrosite/Rhodochrosite.java
+++ src/main/java/net/kalaa/rhodochrosite/Rhodochrosite.java
@@ -3,10 +3,12 @@ package net.kalaa.rhodochrosite;
 import net.fabricmc.api.ModInitializer;
 
 import net.kalaa.rhodochrosite.block.ModBlocks;
+import net.kalaa.rhodochrosite.item.ModItemGroups;
 import net.kalaa.rhodochrosite.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+ //Very important comment
 public class Rhodochrosite implements ModInitializer {
 	public static final String MOD_ID = "rhodochrosite";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -15,5 +17,6 @@ public class Rhodochrosite implements ModInitializer {
 	public void onInitialize() {
 		ModBlocks.registerModBlocks();
 		ModItems.registerModItems();
+		ModItemGroups.registerItemGroups();
 	}
 }
---
+++ src/main/java/net/kalaa/rhodochrosite/item/ModItemGroups.java
@@ -0,0 +1,37 @@
+package net.kalaa.rhodochrosite.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.kalaa.rhodochrosite.Rhodochrosite;
+import net.kalaa.rhodochrosite.block.ModBlocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup RHODOCHROSITE_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(Rhodochrosite.MOD_ID, "rhodochrosite_items"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.RHODOCHROSITE))
+                    .displayName(Text.translatable("itemgroup.rhodochrosite.rhodochrosite_items"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModBlocks.RHODOCHROSITE_BLOCK);
+                        entries.add(ModItems.RAW_RHODOCHROSITE);
+                    }).build());
+
+    public static final ItemGroup RHODOCHROSITE_BLOCKS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(Rhodochrosite.MOD_ID, "rhodochrosite_blocks"),
+            FabricItemGroup.builder().icon(() -> new ItemStack(ModBlocks.RHODOCHROSITE_BLOCK))
+                    .displayName(Text.translatable("itemgroup.rhodochrosite.rhodochrosite_blocks"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModBlocks.RHODOCHROSITE_BLOCK);
+                        entries.add(ModBlocks.RAW_RHODOCHROSITE_BLOCK);
+                    }).build());
+
+
+
+    public static void registerItemGroups() {
+        Rhodochrosite.LOGGER.info("Registering Item Groups for " + Rhodochrosite.MOD_ID);
+    }
+}
