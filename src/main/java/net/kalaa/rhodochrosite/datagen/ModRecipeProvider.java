e66357f56949966a080a9df8de99928484cd1bd0
KalaaCode <mistynmika@gmail.com>
2026-03-25 18:28:48 +0400

    last data gen run done

--- src/main/java/net/kalaa/rhodochrosite/datagen/ModRecipeProvider.java
+++ src/main/java/net/kalaa/rhodochrosite/datagen/ModRecipeProvider.java
@@ -2,6 +2,7 @@ package net.kalaa.rhodochrosite.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.kalaa.rhodochrosite.Rhodochrosite;
 import net.kalaa.rhodochrosite.block.ModBlocks;
 import net.kalaa.rhodochrosite.item.ModItems;
 import net.minecraft.data.server.recipe.RecipeExporter;
@@ -10,6 +11,7 @@ import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.Identifier;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -27,8 +29,6 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         offerSmelting(exporter, RHODOCHROSITE_SMELTABLES, RecipeCategory.MISC, ModItems.RHODOCHROSITE, 0.7f, 200, "rhodochrosite");
         offerBlasting(exporter, RHODOCHROSITE_SMELTABLES, RecipeCategory.MISC, ModItems.RHODOCHROSITE, 0.7f, 100, "rhodochrosite");
 
-        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ModItems.RHODOCHROSITE, RecipeCategory.DECORATIONS, ModBlocks.RHODOCHROSITE_BLOCK);
-
         ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.RHODOCHROSITE_BLOCK)
                 .pattern("RRR")
                 .pattern("RRR")
@@ -40,19 +40,19 @@ public class ModRecipeProvider extends FabricRecipeProvider {
         ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.RHODOCHROSITE, 9)
                 .input(ModBlocks.RHODOCHROSITE_BLOCK)
                 .criterion(hasItem(ModBlocks.RHODOCHROSITE_BLOCK), conditionsFromItem(ModBlocks.RHODOCHROSITE_BLOCK))
-                .offerTo(exporter);
+                .offerTo(exporter, Identifier.of(Rhodochrosite.MOD_ID, "rhodochrosite"));
 
         ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.RAW_RHODOCHROSITE_BLOCK)
-                .pattern("RRR")
-                .pattern("RRR")
-                .pattern("RRR")
-                .input('R', ModItems.RAW_RHODOCHROSITE)
+                .pattern("###")
+                .pattern("###")
+                .pattern("###")
+                .input('#', ModItems.RAW_RHODOCHROSITE)
                 .criterion(hasItem(ModItems.RAW_RHODOCHROSITE), conditionsFromItem(ModItems.RAW_RHODOCHROSITE))
                 .offerTo(exporter);
 
         ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.RAW_RHODOCHROSITE, 9)
                 .input(ModBlocks.RAW_RHODOCHROSITE_BLOCK)
                 .criterion(hasItem(ModBlocks.RAW_RHODOCHROSITE_BLOCK), conditionsFromItem(ModBlocks.RAW_RHODOCHROSITE_BLOCK))
-                .offerTo(exporter);
+                .offerTo(exporter, Identifier.of(Rhodochrosite.MOD_ID, "raw_rhodochrosite"));
     }
 }
